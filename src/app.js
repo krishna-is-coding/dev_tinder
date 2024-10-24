299126e738431c678d3cfc17521fdc0c59ead118
Krishna Sharma <sharma214krishna@gmail.com>
2024-10-24 07:57:45 +0530

    Updated file with changes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+
+    res.send({ name: "Krishna", lastname: "sharma" });
+});
 app.use("/test", (req, res) => {
-  res.send("hi.............server.......");
+    res.send("hi.............server.......");
 });
 app.listen(3000, () => {
-  console.log("sever is running on the port 3000");
+    console.log("sever is running on the port 3000");
 });
