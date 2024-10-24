32a6d70544546b28be532f4c51a07fef990dfe22
Krishna Sharma <sharma214krishna@gmail.com>
2024-10-24 07:49:28 +0530

    server

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/test", (req, res) => {
-  res.send("hi.............dndfhuhfh.......");
+  res.send("hi.............server.......");
 });
 app.listen(3000, () => {
   console.log("sever is running on the port 3000");
