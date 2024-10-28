9d0e89f6048869efdbf90ec459e0da9b1c54feb8
Krishna Sharma <sharma214krishna@gmail.com>
2024-10-28 16:41:01 +0530

    Curd operation of api

--- src/app.js
+++ src/app.js
@@ -6,19 +6,90 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+// for the dynamic databases
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
-  //   const user = new User({
-  //     firstName: "gopi",
-  //     lastName: "Sharma",
-  //     emailId: "gopi.com",
-  //     password: "gopi@123",
-  //   });
+
   try {
     await user.save();
-    res.send("User Added Succesfully......");
+    res.send("User Added Successfully...");
   } catch (err) {
-    res.status(400).send("Error saving the user + err.message");
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+// Get user by email
+app.post("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!userclear) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findOneAndDelete(
+      { _id: userId }, // Condition to match the document
+      {
+        projection: { firstName: 1 }, // Returns only `name` and `email` fields of the deleted doc
+        sort: { createdAt: -1 }, // If there were multiple matches, sort to delete the most recent
+      }
+    );
+    if (user) {
+      res.send("User deleted successfully");
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: userId }, // Query condition
+      data, // Data to update
+      {
+        runValidators: true, // Apply schema validators to the update
+      }
+    );
+    console.log(user);
+
+    res.send("Updated Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Get all users (feed)
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
